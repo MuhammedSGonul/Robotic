fab6d284a4aad9a9053beb55cdd149d5f5578ffd
Muhammed <40292426+MuhammedSGonul@users.noreply.github.com>
2022-02-25 02:52:02 +0300

    Create Send.ino

---
+++ WithoutLibrary/ByteTransmissionBroadcast/Send/Send.ino
@@ -0,0 +1,55 @@
+//////////////////////////////////////////////////////////////////////////
+/*          Muhammed Said Gönül
+   
+    Tested on EBYTE - E32 868T20D SX1276 module.
+   
+    LoRa Pinouts    /   Arduino Pinouts
+ *      GND     ------->      GND      
+ *      VCC     ------->      5V
+ *      AUX     ------->      
+ *      TXD     ------->      D3
+ *      RXD     ------->      D2
+ *      M1      ------->      D4
+ *      M0      ------->      D5
+
+  Notice: If you are testing in Arduino MEGA and not working, 
+  try this pin configuration : RXD ----> D10, TXD ----> D11.
+ 
+ */
+//////////////////////////////////////////////////////////////////////////
+
+#include <SoftwareSerial.h>
+
+#define PIN_RX 3
+#define PIN_TX 2
+#define PIN_M0 4
+#define PIN_M1 5
+#define PIN_AX 6
+
+
+uint8_t Data;
+SoftwareSerial ESerial(PIN_RX, PIN_TX);
+
+
+
+void setup() {
+  pinMode(PIN_AX, INPUT);
+  pinMode(PIN_M0, OUTPUT);
+  pinMode(PIN_M1, OUTPUT);
+
+
+  Serial.begin(9600);
+  ESerial.begin(9600);
+  Serial.println("Starting sender...");
+  delay(1500);
+
+}
+
+void loop() {
+  Data++;
+  ESerial.write((uint8_t*) &Data, sizeof(Data));
+  Serial.print("Sending: "); Serial.println(Data);
+
+  delay(160);   //  It transmits at a frequency of 6.25 Hz.
+
+}
